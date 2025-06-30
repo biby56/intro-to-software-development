a81de6ef9763eaf818def740ed9558752f95b502
biby56 <biby@equinor.com>
2025-06-30 12:54:46 +0000

    Add gardners equation

--- src/geo_calculator/calculations.py
+++ src/geo_calculator/calculations.py
@@ -3,3 +3,7 @@ def find_average(numbers: list[float]) -> float:
     if not numbers:
         raise ValueError("List of numbers is empty")
     return sum(numbers) / len(numbers)
+
+
+def gardners_equation(velocity: float, alpha: float = 0.31, beta: float = 0.25) -> float:
+    return alpha*velocity**beta
--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,4 +1,5 @@
-from geo_calculator.calculations import find_average
+import pytest
+from geo_calculator.calculations import *
 
 
 def test_length_of_string() -> None:
@@ -11,3 +12,10 @@ def test_find_average() -> None:
     numbers = [2, 4, 6, 8, 10]
     expected_average = 6.0
     assert find_average(numbers) == expected_average
+
+def test_gardners_equation():
+    velocity = 2000  # m/s
+    expected_density = 2.0730949  # g/cm3
+
+    # By default, approx considers numbers within a relative tolerance of 1e-6
+    assert gardners_equation(velocity) == pytest.approx(expected_density)
