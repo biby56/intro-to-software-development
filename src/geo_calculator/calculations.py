e6f406eb4393da926215ec010176edbcfa872f29
biby56 <biby@equinor.com>
2025-06-30 13:28:47 +0000

    Add docstrings

--- src/geo_calculator/calculations.py
+++ src/geo_calculator/calculations.py
@@ -6,4 +6,17 @@ def find_average(numbers: list[float]) -> float:
 
 
 def gardners_equation(velocity: float, alpha: float = 0.31, beta: float = 0.25) -> float:
+    """Calculate bulk density using Gardner's equation.
+
+    Args:
+        velocity (float): P-wave velocity in meters per second.
+        alpha (float): Empirical constant, 0.31.
+        beta (float): Empirical constant, 0.25.
+    Returns:
+        float: Bulk density
+    Raises:
+        ValueError: If velocity is negative.
+    """
     return alpha*velocity**beta
+
+
